26ee00682b32b6d75a531688fa297123f1f50e9b
Filipp Lepalaan <filipp@mac.com>
2010-11-13 18:41:46 +0200

    Added readme

--- BatchDMG.m
+++ BatchDMG.m
@@ -1,11 +1,17 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
 @interface Imager : NSObject
 {
-  BOOL g_observing;
-  NSString *destination;
-  NSMutableArray *sources;
+  BOOL g_observing;         // a flag to let us know if we're imaging
+  NSString *destination;    // destination folder
+  NSMutableArray *sources;  // list of volumes being imaged
 }
 
 - (void)observeWorkspace: (NSNotification*) notification;
@@ -121,7 +127,7 @@
 
 @end
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   
---
+++ Readme.md
@@ -0,0 +1,21 @@
+## Description
+BatchDMG is a tool for ripping large collections of discs. Just start the tool and keep 
+inserting discs. The disc is ejected after a rip.
+
+You can use as many optical drives as you want. :-)
+
+
+## License
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
